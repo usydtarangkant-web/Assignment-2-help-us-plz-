59722ba9a8ff977cad7360400eafa955aebb0add
jenniferzhang <jenniferrzhang03@gmail.com>
2026-04-19 15:16:18 +1000

    Comments added to code

--- A2_7.4/Src/main.c
+++ A2_7.4/Src/main.c
@@ -1,56 +1,59 @@
-#include "stm32f303xc.h"
-#include "compass.h"
-#include "compass_led.h"
+#include "stm32f303xc.h"      // Include STM32 register definitions
+#include "compass.h"          // Include compass sensor functions and structure
+#include "compass_led.h"      // Include optional LED display functions
 
 
-/* Uncomment to enable LED compass display */
+/* Uncomment this line to enable LED heading display */
 //#define USE_LEDS
 
 
-/* Global millisecond timer */
+/* Global millisecond timer variable */
 volatile uint32_t tick_ms = 0;
 
 
-/* Global compass structure */
+/* Global structure used to store compass data */
 CompassData compass;
 
 
-/* SysTick interrupt every 1 ms */
+/* SysTick interrupt runs every 1 millisecond */
 void SysTick_Handler(void)
 {
-    tick_ms++;
+    tick_ms++;                // Increase timer count
 }
 
 
+/* Main program */
 int main(void)
 {
-    /* Enable floating point unit */
+    /* Enable floating point unit for atan2 calculations */
     SCB->CPACR |= (0xFUL << 20);
 
-    /* Configure SysTick for 1 ms interrupts */
+    /* Configure SysTick to interrupt every 1 ms */
     SysTick_Config(8000);
 
-    /* Initialise compass */
+    /* Initialise onboard compass sensor */
     compassInit();
 
 
 #ifdef USE_LEDS
+    /* Initialise LEDs if enabled */
     compassLEDInit();
 #endif
 
 
     while (1)
     {
-        /* Update compass data */
+        /* Read latest compass values into structure */
         compassRead(&compass);
 
 
 #ifdef USE_LEDS
+        /* Display heading on LEDs if enabled */
         compassLEDDisplay(&compass);
 #endif
 
 
-        /* Small delay */
+        /* Small delay to slow update rate */
         for (volatile int i = 0; i < 100000; i++);
     }
 }
